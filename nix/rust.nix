1c278a17cfbfa754132a2989b90ff7f513fdd8c8
Daniel Lütgehetmann <daniel.luetgehetmann@inait.ai>
2023-08-25 15:38:33 +0200

    Push

--- nix/rust.nix
+++ nix/rust.nix
@@ -1,5 +1,5 @@
 { system, pkgs, packageName, crane, python
-, rustToolchain ? (pkgs.rust-bin.stable.latest.default) }:
+, rustToolchain ? (pkgs.rust-bin.fromRustupToolchainFile ../rust-toolchain.toml) }:
 let
   toolchain = rustToolchain.override {
     extensions = [
